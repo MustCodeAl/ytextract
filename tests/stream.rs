917f3e47ced0a6db333470476c2ed32752633968
Tilmann Meyer <me@atiltedtree.dev>
2021-07-31 15:56:09 +0200

    test(steams): Add error handling tests
    Signed-off-by: Tilmann Meyer <me@atiltedtree.dev>

--- tests/stream.rs
+++ tests/stream.rs
@@ -22,7 +22,6 @@ macro_rules! define_test {
 }
 
 define_test!(normal, "9bZkp7q19f0");
-define_test!(unlisted, "ZGdLIwrGHG8");
 define_test!(live_stream_recording, "rsAAeyAr-9Y");
 define_test!(high_quality_streams, "V5Fsj_sCKdg");
 define_test!(dash_manifest, "AI7ULzgf8RU");
@@ -37,4 +36,47 @@ mod embed_restricted {
 
     define_test!(youtube, "_kmeFXjjGfk");
     define_test!(author, "MeJVWBSsPAY");
+}
+
+mod error {
+    use super::CLIENT;
+
+    macro_rules! define_test {
+        ($fn:ident, $id:literal, $error:ident) => {
+            #[async_std::test]
+            async fn $fn() -> Result<(), Box<dyn std::error::Error>> {
+                let id = $id.parse()?;
+                assert!(matches!(
+                    CLIENT.streams(id).await,
+                    Err(ytextract::Error::Youtube(ytextract::error::Youtube::$error)),
+                ));
+                Ok(())
+            }
+        };
+    }
+
+    define_test!(not_found, "L_VmQZtLVID", NotFound);
+    define_test!(private, "ZGdLIwrGHG8", Private);
+    define_test!(required_purchase, "p3dDcKOFXQg", PurchaseRequired);
+    define_test!(
+        nudity_or_sexual,
+        "-JVFs5w9V0U",
+        NudityOrSexualContentViolation
+    );
+    define_test!(account_terminated, "Pfhpe6shO2U", AccountTerminated);
+    define_test!(removed_by_uploader, "N3QlpdWUpHo", RemovedByUploader);
+    define_test!(tos_violation, "tJievCeKBs0", TermsOfServiceViolation);
+    #[async_std::test]
+    async fn copyright_claim() -> Result<(), Box<dyn std::error::Error>> {
+        let id = "6MNavkSGntQ".parse()?;
+
+        match CLIENT.streams(id).await {
+            Ok(_) => panic!("got OK"),
+            Err(ytextract::Error::Youtube(ytextract::error::Youtube::CopyrightClaim {
+                claiment,
+            })) if claiment == "Richard DiBacco" => {}
+            Err(other) => panic!("{:#?}", other),
+        }
+        Ok(())
+    }
 }
